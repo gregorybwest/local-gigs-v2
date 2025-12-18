b7090823ff59c281f18d5d908bb2e96879fc80cf
Greg West <gregorybwest@gmail.com>
2025-12-17 21:21:37 -0800

    user has many events

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :events, dependent: :destroy
+  
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true
   validates :user_name, presence: true
