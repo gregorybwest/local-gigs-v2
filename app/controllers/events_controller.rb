25bf6453d4b7963abb05e98cd0411c25151af7fa
Greg West <gregorybwest@gmail.com>
2025-12-17 21:13:15 -0800

    scaffold event model

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,58 @@
+class EventsController < ApplicationController
+  before_action :set_event, only: %i[ show edit update destroy ]
+
+  # GET /events
+  def index
+    @events = Event.all
+  end
+
+  # GET /events/1
+  def show
+  end
+
+  # GET /events/new
+  def new
+    @event = Event.new
+  end
+
+  # GET /events/1/edit
+  def edit
+  end
+
+  # POST /events
+  def create
+    @event = Event.new(event_params)
+
+    if @event.save
+      redirect_to @event, notice: "Event was successfully created."
+    else
+      render :new, status: :unprocessable_content
+    end
+  end
+
+  # PATCH/PUT /events/1
+  def update
+    if @event.update(event_params)
+      redirect_to @event, notice: "Event was successfully updated.", status: :see_other
+    else
+      render :edit, status: :unprocessable_content
+    end
+  end
+
+  # DELETE /events/1
+  def destroy
+    @event.destroy!
+    redirect_to events_path, notice: "Event was successfully destroyed.", status: :see_other
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_event
+      @event = Event.find(params.expect(:id))
+    end
+
+    # Only allow a list of trusted parameters through.
+    def event_params
+      params.expect(event: [ :mapbox_id, :show_time, :flier_image_url, :ticket_link_url, :user_id ])
+    end
+end
