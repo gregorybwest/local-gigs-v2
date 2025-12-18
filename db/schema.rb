99409ee9fcf86da6b162c854d29618d26c367f10
Greg West <gregorybwest@gmail.com>
2025-12-17 20:55:58 -0800

    Regenerate schema after migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 0) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_18_043711) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "users", force: :cascade do |t|
+    t.text "bio"
+    t.datetime "created_at", null: false
+    t.string "email", null: false
+    t.string "image_url"
+    t.boolean "is_artist", default: false, null: false
+    t.string "password", null: false
+    t.string "preferred_location", null: false
+    t.string "spotify_link"
+    t.datetime "updated_at", null: false
+    t.string "user_name", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
 end
