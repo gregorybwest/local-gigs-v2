f62a1dd6deb56baa95b7ac71056b56a06e2914bc
Greg West <gregorybwest@gmail.com>
2026-01-14 19:37:15 -0800

    add validations for mapbox id and coordaintes

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -2,4 +2,6 @@ class Venue < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
   validates :city, presence: true
+  validates :coordinates, presence: true
+  validates :mapbox_id, presence: true
 end
