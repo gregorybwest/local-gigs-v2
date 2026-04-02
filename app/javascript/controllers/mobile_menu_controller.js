5b030e20fed3c6c5a1b0975ab61d060130b1e03d
Stefan Webster <swebster08@gmail.com>
2026-04-01 21:11:40 -0700

    Mobile UI Improvements

--- app/javascript/controllers/mobile_menu_controller.js
+++ app/javascript/controllers/mobile_menu_controller.js
@@ -1,10 +1,11 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["menu"];
+  static targets = ["menu", "searchPanel"];
 
   toggle() {
     const menu = this.menuTarget;
+    this.closeSearch();
 
     if (menu.classList.contains("hidden")) {
       menu.classList.remove("hidden");
@@ -20,6 +21,35 @@ export default class extends Controller {
         menu.classList.add("hidden");
         menu.classList.remove("flex");
       }, 200);
+    }
+  }
+
+  toggleSearch() {
+    if (!this.hasSearchPanelTarget) return;
+    const panel = this.searchPanelTarget;
+
+    // Close burger menu if open
+    const menu = this.menuTarget;
+    if (!menu.classList.contains("hidden")) {
+      menu.classList.remove("opacity-100", "scale-100");
+      menu.classList.add("opacity-0", "scale-95");
+      setTimeout(() => {
+        menu.classList.add("hidden");
+        menu.classList.remove("flex");
+      }, 200);
+    }
+
+    if (panel.classList.contains("hidden")) {
+      panel.classList.remove("hidden");
+      setTimeout(() => panel.querySelector("input")?.focus(), 50);
+    } else {
+      this.closeSearch();
+    }
+  }
+
+  closeSearch() {
+    if (this.hasSearchPanelTarget) {
+      this.searchPanelTarget.classList.add("hidden");
     }
   }
 }
